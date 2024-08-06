4254dc929f6e02547b04ac6f46d4f7d07f9a53d7
linxin <lxinin1999@gmail.com>
2024-08-06 19:56:52 +0800

    Connnecting Java and DB, execute and process

---
+++ src/Demojdbc.java
@@ -0,0 +1,40 @@
+import java.sql.*;
+
+public class Demojdbc {
+    public static void main(String[] args) throws Exception {
+
+        /*
+            import package
+            load and register
+            create connection
+            create statement
+            execute statement
+            process the results
+            close
+         */
+
+
+
+        Class.forName("org.postgresql.Driver");
+
+        String url = "jdbc:postgresql://localhost:5432/Demo";
+        String username = "postgres";
+        String password = "0000";
+        Connection con = DriverManager.getConnection(url, username, password);
+
+        System.out.println("Connected to PostgreSQL database");
+
+        Statement stmt = con.createStatement();
+
+        String sql = "select * from student";
+        ResultSet rs = stmt.executeQuery(sql);
+
+        rs.next();
+        String name = rs.getString("sname"); // get the specific column
+        System.out.println("Name of a student is " + name);
+
+        con.close();
+
+        System.out.println("Connection closed");
+    }
+}

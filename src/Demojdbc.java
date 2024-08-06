03fe55649ceca89b76cc02931b41f9826ca0353f
linxin <lxinin1999@gmail.com>
2024-08-06 20:43:34 +0800

    Crud operations

--- src/Demojdbc.java
+++ src/Demojdbc.java
@@ -26,18 +26,31 @@ public class Demojdbc {
 
         Statement stmt = con.createStatement();
 
-        String sql = "select * from student";
-        ResultSet rs = stmt.executeQuery(sql);
+//        String sql = "select * from student";
+//        ResultSet rs = stmt.executeQuery(sql);
+//
+////        rs.next();
+////        String name = rs.getString("sname"); // get the specific column
+////        System.out.println("Name of a student is " + name);
+//
+//        while (rs.next()) {
+//            System.out.println(rs.getInt(1) + " - "
+//                    + rs.getString(2) + " - "
+//                    + rs.getString(3));
+//        }
 
-//        rs.next();
-//        String name = rs.getString("sname"); // get the specific column
-//        System.out.println("Name of a student is " + name);
+        // insert operation
+//        String sql = "insert into student values(5, 'John', 48)";
+//        boolean status = stmt.execute(sql);
+//        System.out.println(status); // the operation is done, but the status is false
 
-        while (rs.next()) {
-            System.out.println(rs.getInt(1) + " - "
-                    + rs.getString(2) + " - "
-                    + rs.getString(3));
-        }
+        // update operation
+//        String sql = "update student set sname='Max' where sid=5";
+//        stmt.executeUpdate(sql);
+
+        // delete operation
+        String sql = "delete from student where sid=5";
+        stmt.executeUpdate(sql);
 
         con.close();
 
